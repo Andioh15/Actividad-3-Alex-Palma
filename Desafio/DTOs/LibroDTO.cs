21571161d7066e6a4e197a1d5f9ab41a47173982
ALEX <151946798+Andioh15@users.noreply.github.com>
2026-07-15 22:00:26 -0500

    Delete Desafio directory

--- Desafio/DTOs/LibroDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace DesafioFinal.DTOs
-{
-    public class LibroDTO
-    {
-        public string Titulo { get; set; } = string.Empty;
-        public string Estado { get; set; } = string.Empty; // Dirá "Disponible" o "Prestado"
-    }
-}
--- Desafio/DTOs/UsuarioDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace DesafioFinal.DTOs
-{
-    public class UsuarioDTO
-    {
-        // A veces no queremos enviar el ID ni contraseñas, solo info básica
-        public string Nombre { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-    }
-}
